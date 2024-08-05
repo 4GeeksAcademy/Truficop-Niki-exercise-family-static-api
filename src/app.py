b29fa9326d28684b9e3e91a92b0458a6a143d768
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-08-05 01:30:25 +0000

    Hecho

--- src/app.py
+++ src/app.py
@@ -31,12 +31,35 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['GET'])
+def get_all_members():
+    members = jackson_family.get_all_members()
+    print(members)  # this will print the list of members in the console
+    return jsonify(members), 200
+
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"error": "Miembro no encontrado"}), 404
+    
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+    new_member = jackson_family.add_member(member)
+    return jsonify(new_member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted_member = jackson_family.delete_member(member_id)
+    return jsonify(deleted_member), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
